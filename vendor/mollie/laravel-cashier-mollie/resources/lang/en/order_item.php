397d029d96defbd51a97ebda398fe5acef615f5f
Ruveydakartal <ruveyda.kartal@outlook.be>
2023-10-10 13:20:33 +0200

    commit

--- vendor/mollie/laravel-cashier-mollie/resources/lang/en/order_item.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'cycle' => 'From :from to :to',
-];
--- vendor/mollie/laravel-cashier-mollie/src/Refunds/RefundCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Laravel\Cashier\Refunds;
-
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
-
-class RefundCollection extends EloquentCollection
-{
-}
